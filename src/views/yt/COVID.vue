17470cd56370a246aa9edbdda84638e69537fa7d
Levi Richardson <litbelb1@outlook.com>
2021-04-20 11:00:30 -0700

    Add COVID-19 YouTube info files

---
+++ src/views/yt/COVID.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="root">
+    <h2>COVID-19 Information</h2>
+    <hr />
+    <h2 class="cloak-2">Local laws and guidelines</h2>
+    <p>Most of my videos are filmed entirely in King County of Washington State, which is currently in <a href="https://www.coronavirus.wa.gov/what-you-need-know/county-status-and-safe-start-application-process">phase 3</a>
+    of Governor Inslee's <a href="https://coronavirus.wa.gov/what-you-need-know/safe-start/whats-open">Safe Start program.</a> That means that I can have an outdoor social gathering of up to
+    50 people outside of my own household, and 10 people while indoors. In addition, State, County, and City-owned outdoor Entertainment establishments are at a maximum of 400 people, or "spectators".
+    Masks are also required indoors at all times outside of your home, and outdoors <strong>IF</strong> it is not possible/reasonable to be 6 feet apart.</p>
+    <h2 class="cloak-3">My own comments</h2>
+    <p>
+        <ul>Both my Mother and Brother are fully vaccinated now.</ul>
+        <ul>I am never within 6 feet of someone outside of my own household</ul>
+        <ul>All reasonable areas are sanitized thouroughly after use</ul>
+    </p>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+export default class Home extends Vue {}
+</script>
---
+++ src/views/yt/covid-videoId734198.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="root">
+    <h2>COVID-19 Information</h2>
+    <h4>This video is about <a href="https://huelet.net/w/">"When the Coffee Kicks in"</a> specifically. If you'd like more general Information on how I'm following local policies, please go to my <a href="/yt/COVID">COVID-19</a> page.</h4>
+    <hr />
+    <h2 class="cloak-2">Local laws and guidelines</h2>
+    <p>This video was filmed entirely in King County of Washington State, which is currently in <a href="https://www.coronavirus.wa.gov/what-you-need-know/county-status-and-safe-start-application-process">phase 3</a>
+    of Governor Inslee's <a href="https://coronavirus.wa.gov/what-you-need-know/safe-start/whats-open">Safe Start program.</a> That means that I can have an outdoor social gathering of up to
+    50 people outside of my own household, and 10 people while indoors. In addition, State, County, and City-owned outdoor Entertainment establishments are at a maximum of 400 people, or "spectators".
+    Masks are also required indoors at all times outside of your home, and outdoors <strong>IF</strong> it is not possible/reasonable to be 6 feet apart. In this video, I interacted with 1 person
+    outside of my home, outdoors, with a mask on, meaning I exceeded all guidelines.</p>
+    <h2 class="cloak-3">My own comments</h2>
+    <p>
+        <ul>Both my Mother and Brother are fully vaccinated at the time of publishing.</ul>
+        <ul>I was never within 6 feet of someone outside of my own household</ul>
+        <ul>All reasonable areas were sanitized thouroughly after use</ul>
+    </p>
+</div>
+</template>
+<style>
+p {
+    text-align: left;
+}
+</style>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+export default class Home extends Vue {}
+</script>
