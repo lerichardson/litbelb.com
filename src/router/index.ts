f8704088c5ff4ffe72e19a202dc1053bc1d8c71a
Levi Richardson <litbelb1@outlook.com>
2021-04-20 11:01:30 -0700

    Incorporate COVID-19 YouTube info files

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,22 +17,32 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/About.vue"),
   },
   {
     path: "/medium",
     name: "Medium",
-    component: () => import("../views/medium.vue"),
+    component: () => import("@/views/medium.vue"),
   },
   {
     path: "/projects",
     name: "projects",
-    component: () => import("../views/My-Projects.vue"),
+    component: () => import("@/views/My-Projects.vue"),
   },
   {
     path: "/Privacy",
     name: "privacy",
     component: () => import("@/views/Privacy.vue"),
+  },
+  {
+    path: "/yt/covid-videoId734198",
+    name: "covid-videoId734198",
+    component: () => import("@/views/yt/covid-videoId734198.vue"),
+  },
+  {
+    path: "/yt/COVID",
+    name: "COVID",
+    component: () => import("@/views/yt/COVID.vue"),
   }
 ];
 
