7d3a58df1bcc9071f47f5f9759d38fe7aabe0f97
Levi <litbelb1@outlook.com>
2022-06-28 13:56:02 +0100

    Revert "Update App.vue"
    This reverts commit 454bc038751e68d3b720a89bfce8b23d7b09e82e.

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,6 @@
     <router-view />
     <div id="footer">
       <router-link to="/Privacy">Privacy</router-link> |
-      <router-link to="/Medium">Writings</router-link> |
-      <a href="https://github.com/lerichardson/litbelb.com"
-        >Edit this page on GitHub</a
-      >
       <router-link to="/Medium">Writings</router-link>
     </div>
   </div>
